cac7054ec5c8a55a78a9475a37a2caabbd911a4c
Malgorzata Flejter <malgorzata.flejter@gmail.com>
2022-01-23 11:35:09 +0100

    js added

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const btn = document.querySelector('header a');
+const h1 = document.querySelector('header h1')
+let coutner = 0;
+btn.addEventListener('click', () => {
+   coutner++;
+   // alert('test');
+   console.log(coutner);
+   if(coutner % 2 === 0) {
+      h1.innerHTML = 'Pierwsza strona';
+   } else {
+   h1.innerHTML = 'Będzie Pan zadowolony';
+}
+});
